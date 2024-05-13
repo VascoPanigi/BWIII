e8cd5b31e402b10b0a1ff1a1a4ed423bc2fda932
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-05-13 22:46:39 +0200

    fixed style

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,40 +1,48 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { Image } from "react-bootstrap";
-import bell from "../assets/icons/bell.svg";
-import grid from "../assets/icons/grid.svg";
-import messaging from "../assets/icons/messaging.svg";
-import search from "../assets/icons/search.svg";
-import network from "../assets/icons/network.svg";
-import home from "../assets/icons/home.svg";
-import jobs from "../assets/icons/jobs.svg";
-import Form from "react-bootstrap/Form";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import logo from "../assets/icons/logo.svg";
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import { Image } from 'react-bootstrap'
+import bell from '../assets/icons/bell.svg'
+import grid from '../assets/icons/grid.svg'
+import messaging from '../assets/icons/messaging.svg'
+import network from '../assets/icons/network.svg'
+import home from '../assets/icons/home.svg'
+import jobs from '../assets/icons/jobs.svg'
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import logo from '../assets/icons/logo.svg'
+import propic from '../assets/img/propic.jpg'
 
 const MyNavbar = () => {
   return (
     <Navbar expand="lg" className="navbar-container bg-body-tertiary align-items-center">
       <Container className="justify-content-between">
-        <div className="d-flex align-items-center">
+        <div className="d-flex align-items-center gap-1">
           <Navbar.Brand href="#home">
-            <Image src={logo}></Image>
+            <Image src={logo} />
           </Navbar.Brand>
           <Form>
-            <Row>
-              <Col xs="auto">
-                <Form.Control type="text" placeholder="Search" className=" mr-sm-2" />
-              </Col>
-            </Row>
+            <Col className="p-0 d-none d-md-block">
+              <div className="input-group">
+                <span className="input-group-text" id="basic-addon1">
+                  <i className="bi bi-search"></i>
+                </span>
+                <Form.Control
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2"
+                  aria-label="Search"
+                  aria-describedby="basic-addon1"
+                />
+              </div>
+            </Col>
           </Form>
         </div>
         <div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="navbar-group me-auto">
               <Nav.Link className="text-center" href="#home">
                 <Image src={home}></Image>
                 <p>Home</p>
@@ -55,26 +63,32 @@ const MyNavbar = () => {
                 <Image src={bell}></Image>
                 <p>Notifications</p>
               </Nav.Link>
-              <NavDropdown title="Me" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-              <NavDropdown title="For Business" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+              <div className="d-flex flex-column justify-content-end align-items-center" style={{ maxWidth: '78px' }}>
+                <Image src={propic} className="propic mx-auto" />
+                <NavDropdown title="Me" id="basic-nav-dropdown" className="first-dropdown" align="end">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </div>
+              <div className="separate d-flex flex-column justify-content-end align-items-center">
+                <Image src={grid} className="mx-auto" />
+                <NavDropdown title="For Business" id="basic-nav-dropdown" className="second-dropdown" align="end">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </div>
             </Nav>
           </Navbar.Collapse>
         </div>
       </Container>
     </Navbar>
-  );
-};
+  )
+}
 
-export default MyNavbar;
+export default MyNavbar
