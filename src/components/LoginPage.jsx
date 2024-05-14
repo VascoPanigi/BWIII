1fde0e28f358449c8c2ebfbba63f0d950c442835
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-05-14 22:42:40 +0200

    small fixes

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,57 +1,59 @@
-import { useState } from "react";
-import { Col, Row, Form, Button } from "react-bootstrap";
-import { SET_USER_LOGGED, TOGGLE_IS_LOGGED } from "../redux/actions";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react'
+import { Col, Row, Form, Button } from 'react-bootstrap'
+import { SET_USER_LOGGED, TOGGLE_IS_LOGGED } from '../redux/actions'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const PlaintextExample = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    if (email === "ciao@ciao.it" && password === "ciao") {
-      const id = "6642750c55621a0015c15faa";
-      console.log(id);
+    e.preventDefault()
+    if (email === 'ciao@ciao.it' && password === 'ciao') {
+      const id = '6642750c55621a0015c15faa'
+      console.log(id)
 
-      dispatch({ type: TOGGLE_IS_LOGGED, payload: true });
-      dispatch({ type: SET_USER_LOGGED, payload: id });
-      navigate(`/user/${id}`);
+      dispatch({ type: TOGGLE_IS_LOGGED, payload: true })
+      dispatch({ type: SET_USER_LOGGED, payload: id })
+      navigate(`/user/${id}`)
     }
-  };
+  }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="name@example.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </Form.Group>
-
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-        <Form.Label column sm="2">
-          Password
-        </Form.Label>
-        <Col sm="10">
+    <div className="background">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            type="email"
+            placeholder="name@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-        </Col>
-      </Form.Group>
+        </Form.Group>
 
-      <Button type="submit">LOGIN</Button>
-    </Form>
-  );
-};
+        <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+          <Form.Label column sm="2">
+            Password
+          </Form.Label>
+          <Col sm="10">
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Col>
+        </Form.Group>
 
-export default PlaintextExample;
+        <Button type="submit">LOGIN</Button>
+      </Form>
+    </div>
+  )
+}
+
+export default PlaintextExample
