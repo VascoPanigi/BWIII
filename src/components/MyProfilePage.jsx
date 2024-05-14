6f644803dcb467b8e8c077677e81a2958dbcfd92
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-05-14 16:42:01 +0200

    fixes

--- src/components/MyProfilePage.jsx
+++ src/components/MyProfilePage.jsx
@@ -1,38 +1,39 @@
-import { Col, Container, Row } from "react-bootstrap";
-import ProfileHero from "./ProfileHero";
-import MySuggestions from "./Mysuggestions";
-import MyProjects from "./MyProjects";
-import Education from "./Education";
-import Experience from "./Experience";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchProfileAction, fetchUsersListAction, fetchExperiencesAction } from "../redux/actions";
-import { useNavigate, useParams } from "react-router-dom";
+import { Col, Container, Row } from 'react-bootstrap'
+import ProfileHero from './ProfileHero'
+import MySuggestions from './Mysuggestions'
+import MyProjects from './MyProjects'
+import Education from './Education'
+import Experience from './Experience'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProfileAction, fetchUsersListAction, fetchExperiencesAction } from '../redux/actions'
+import { useParams } from 'react-router-dom'
 
 const MyProfilePage = () => {
-  const dispatch = useDispatch();
-  const userData = useSelector((state) => state.user);
-  const usersList = useSelector((state) => state.users_list);
-  const experiencesList = useSelector((state) => state.experiences_list);
+  const dispatch = useDispatch()
+  const userData = useSelector((state) => state.user)
+  const usersList = useSelector((state) => state.users_list)
+  const experiencesList = useSelector((state) => state.experiences_list)
   // const loginStatus = useSelector((state) => state.login);
-  console.log("showUserData", userData);
-  console.log("experiencetest", experiencesList);
+  console.log('showUserData', userData)
+  console.log('experiencetest', experiencesList)
 
-  console.log(usersList);
+  console.log(usersList)
 
-  const loginStatus = useSelector((state) => state.login);
-  console.log(loginStatus);
+  const loginStatus = useSelector((state) => state.login)
+  console.log(loginStatus)
 
-  const params = useParams();
+  const params = useParams()
   // const navigate = useNavigate();
 
-  console.log("ID dinamico", params.dynamicValue);
+  console.log('ID dinamico', params.dynamicValue)
 
   useEffect(() => {
-    dispatch(fetchProfileAction(params.dynamicValue));
-    dispatch(fetchUsersListAction());
-    dispatch(fetchExperiencesAction(params.dynamicValue));
-  }, []);
+    dispatch(fetchProfileAction(params.dynamicValue))
+    dispatch(fetchUsersListAction())
+    dispatch(fetchExperiencesAction(params.dynamicValue))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div className="background">
@@ -50,7 +51,7 @@ const MyProfilePage = () => {
         </Row>
       </Container>
     </div>
-  );
-};
+  )
+}
 
-export default MyProfilePage;
+export default MyProfilePage
