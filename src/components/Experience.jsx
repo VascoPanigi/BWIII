905787a560d6fa39c1a4fd20e8e7099badb18ce9
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-05-15 10:11:22 +0200

    expmodal

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,27 @@
 import { Card } from 'react-bootstrap'
 import ExperienceCard from './ExperienceCard'
+import { useDispatch, useSelector } from 'react-redux'
+import { setModalType, showModal } from '../redux/actions'
+import ExpModal from './ExpModal'
 
 const Experience = ({ experiencesList }) => {
   console.log('experiencestest2', experiencesList)
+
+  const dispatch = useDispatch()
+  const modalType = useSelector((state) => state.modal.modalType)
+  console.log(modalType)
+
+  const handleShowExpModal = () => {
+    dispatch(setModalType('exp'))
+    dispatch(showModal())
+  }
+
   return (
     <Card className="section-container">
       <div className="inner-section-container">
         <div className="edit-wrapper">
-          <i className="edit bi bi-pen"></i>
+          <i className="edit bi bi-pen" onClick={handleShowExpModal}></i>
+          {modalType === 'exp' && <ExpModal userData={userData} />}
         </div>
         <p className=" section-title">Experience</p>
         {experiencesList.experiences_list.map((experience) => (
