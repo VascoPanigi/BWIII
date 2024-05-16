17683dfbfd2953b4693d339681afc59e875b4865
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-05-16 12:20:08 +0200

    small fixes

--- src/components/ExperiencePage.jsx
+++ src/components/ExperiencePage.jsx
@@ -1,27 +1,29 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
-import ExperienceCard from "./ExperienceCard";
-import { useSelector } from "react-redux";
-import MySuggestions from "./Mysuggestions";
-import { useDispatch } from "react-redux";
-import { fetchExperiencesAction, setModalType, showModal } from "../redux/actions";
-import ExpModal from "./ExpModal";
-import { useEffect } from "react";
+import { Card, Col, Container, Row } from 'react-bootstrap'
+import ExperienceCard from './ExperienceCard'
+import { useSelector } from 'react-redux'
+import MySuggestions from './Mysuggestions'
+import { useDispatch } from 'react-redux'
+import { fetchExperiencesAction, setModalType, showModal } from '../redux/actions'
+import ExpModal from './ExpModal'
+import { useEffect } from 'react'
 
 const ExperiencePage = () => {
-  const experiencesList = useSelector((state) => state.experiences_list);
-  const usersList = useSelector((state) => state.users_list);
-  const userId = useSelector((state) => state.user.other_user_info._id);
-  const dispatch = useDispatch();
-  const modalType = useSelector((state) => state.modal.modalType);
+  const experiencesList = useSelector((state) => state.experiences_list)
+  const usersList = useSelector((state) => state.users_list)
+  const userId = useSelector((state) => state.user.other_user_info._id)
+  const dispatch = useDispatch()
+  const modalType = useSelector((state) => state.modal.modalType)
+  const loggedIn = useSelector((state) => state.login.isLogged)
+  console.log('logge', loggedIn)
 
   useEffect(() => {
-    dispatch(fetchExperiencesAction(userId));
-  }, []);
+    dispatch(fetchExperiencesAction(userId))
+  }, [])
 
   const handleShowExpModal = () => {
-    dispatch(setModalType("exp"));
-    dispatch(showModal());
-  };
+    dispatch(setModalType('exp'))
+    dispatch(showModal())
+  }
 
   return (
     <div className="background">
@@ -29,11 +31,14 @@ const ExperiencePage = () => {
         <Row className="justify-content-center gap-2">
           <Col xs={12} md={6} lg={7} className="left-column">
             <Card className="section-container exp-container">
-              {modalType === "exp" && <ExpModal expData={experiencesList} />}
+              {modalType === 'exp' && <ExpModal expData={experiencesList} />}
               <div className="inner-section-container ">
-                <div className="edit-wrapper">
-                  <i className="bi bi-plus-lg" onClick={handleShowExpModal}></i>
-                </div>
+                {loggedIn && (
+                  <div className="edit-wrapper">
+                    <i className="bi bi-plus-lg" onClick={handleShowExpModal}></i>
+                  </div>
+                )}
+
                 <p className=" section-title">Experience</p>
 
                 {experiencesList.experiences_list.length > 1 &&
@@ -49,6 +54,6 @@ const ExperiencePage = () => {
         </Row>
       </Container>
     </div>
-  );
-};
-export default ExperiencePage;
+  )
+}
+export default ExperiencePage
