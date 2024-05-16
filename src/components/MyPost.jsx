c5556bb241f86bb9b6be617624f14ada99a0e711
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-05-16 10:39:51 +0200

    date update

--- src/components/MyPost.jsx
+++ src/components/MyPost.jsx
@@ -1,21 +1,45 @@
-import { Col, Row } from 'react-bootstrap'
-import { useSelector } from 'react-redux'
+import { Col, Row } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const MyPost = () => {
-  const AllPosts = useSelector((state) => state.posts.posts)
+  const AllPosts = useSelector((state) => state.posts.posts);
 
   //   AllPosts &&
   //     AllPosts.slice(-1, -10).map((post) => {
   //       console.log(post);
   //     });
 
+  const data = (data) => {
+    const nowDate = new Date();
+    const newData = new Date(data);
+    const oneDay = 1000 * 60 * 60 * 24;
+    const milliseconds = Math.abs(nowDate - newData);
+    const day = Math.round(milliseconds / oneDay);
+    const seconds = Math.round(milliseconds / 1000);
+    const minutes = Math.round(seconds / 60);
+    const hours = Math.round(minutes / 60);
+    
+    if (seconds < 60) {
+      return seconds + "s";
+    } else if (minutes < 60) {
+      return minutes + " m";
+    } else if (hours < 24) {
+      return hours + " h";
+    } else if (day < 31) {
+      return day + " d";
+    } else if (day < 365) {
+      return Math.round(day / 31) + " m";
+    } else {
+      return Math.floor(day / 365) + " y";
+    }
+  };
   return (
     AllPosts &&
     AllPosts.slice(-40, -25).map((post) => {
-      console.log(post)
+      console.log(post);
       return (
         <>
-          <Row className="border" style={{ marginBottom: '1rem' }}>
+          <Row className="border" style={{ marginBottom: "1rem" }}>
             <Col>
               <Row>
                 <Col xs="3" lg="2">
@@ -25,12 +49,18 @@ const MyPost = () => {
                   <span>{post.user.username}</span>
                   <blockquote className="m-0">{post.user.area}</blockquote>
                   <blockquote className="m-0">
-                    3s <strong>.</strong>
+                    {data(post.createdAt)} <strong>.</strong>
                     <i className="bi bi-globe-americas"></i>
                   </blockquote>
                 </Col>
                 <p>{post.text}</p>
-                {post.image && <img className="post-image img-post" src={post.image} alt="" />}
+                {post.image && (
+                  <img
+                    className="post-image img-post"
+                    src={post.image}
+                    alt=""
+                  />
+                )}
 
                 <Row xs="3" className="text-center">
                   <Col xs="3" className="text-center">
@@ -51,11 +81,11 @@ const MyPost = () => {
                   </Col>
                 </Row>
               </Row>
-            </Col>{' '}
+            </Col>{" "}
           </Row>
         </>
-      )
+      );
     })
-  )
-}
-export default MyPost
+  );
+};
+export default MyPost;
